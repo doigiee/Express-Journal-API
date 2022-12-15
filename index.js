2e079780df8965ae871bba6e5e7b01058e998c78
doigiee <doigiee@live.com>
2022-12-15 10:57:47 +1000

    initial commit

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ import express from 'express'
 
 const categories = ['Food', 'Coding', 'Work', 'Other']
 
-
 const app = express()
-const port = 4001
+const port = 8080
 
-app.get('/', (request, response) => response.send({info: 'Journal API too see me'}))
+app.use(express.json())
 
-app.get('/categories', (req, res) => res.status(204).send(categories))
+app.get('/categories', (req, res) => res.status(200).send(categories))
+const entries = [
+    {category: 'Food', content: 'Hello!'},
+    {category: 'Coding', content: 'Express is cool'},
+    {category: 'Work', content: 'Anphter day at the office'}
+]
 
-app.listen(port, () => console.log(`App is running at http://localhost:${port}`))
+app.get('/entries', (req, res) => res.status(204).send(entries))
+app.post('/entries', (req, res) => {
+console.log(req.body.foo)
+res.send(req.body)
+})
+
+app.get('/', (request, response) => response.send({ info: 'Journal API' }))
+
+app.listen(port, () => console.log(`App running at http://localhost:${port}/`))
