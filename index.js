e64daf20b54caf406eb284e9adb4e4c34443b453
doigiee <doigiee@live.com>
2023-01-16 13:31:42 +1000

    16/01/2023

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-import express from 'express'
-import { CategoryModel } from './db.js'
-import entryRoutes from './routes/entry_routes.js'
-import cors from 'cors'
+import app from './app.js'
 
-const app = express()
 const port = 4001
 
-app.use(cors())
-
-app.use(express.json())
-
-app.get('/', (request, response) => response.send({ info: 'Journal API 2023' }))
-
-app.get('/categories', async (req, res) => res.send(await CategoryModel.find()))
-
-app.use('/entries', entryRoutes)
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}/`))
